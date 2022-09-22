4dcc276528eef778f2d355547357ce4f25473532
Jonathan Bruckman <jbruckman86@gmail.com>
2022-09-21 23:47:42 -0400

    commit tree 2eebda1fe5503c5f50dd2f00c5f9aafb0e79ea56

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield',];
+
+function destructivelyAppendCat(name) {
+    return(name, cats.push('Ralph'));
+}
+
+function destructivelyPrependCat(name) {
+    return(name, cats.unshift('Bob'));
+}
+function destructivelyRemoveLastCat() {
+    return(cats.pop());
+}
+function destructivelyRemoveFirstCat() {
+    return(cats.shift());
+}
+function appendCat() {
+    const newCats = [...cats, 'Broom']
+    return newCats;
+}
+function prependCat() {
+    const newerCats = ['Arnold', ...cats];
+    return newerCats
+}
+function removeLastCat() {
+    return cats.slice(0, 2)
+}
+function removeFirstCat() {
+    return cats.slice(-2)
+}
